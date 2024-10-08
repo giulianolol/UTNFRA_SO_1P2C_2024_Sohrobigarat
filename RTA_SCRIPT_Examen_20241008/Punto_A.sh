16cbff59edde57cdd2f9097ab504396ef6a3b5c9
giulianolol <gino.sohro@yahoo.com.ar>
2024-10-08 16:10:25 +0000

    Add: Punto 1 Resuelto + Script para actualizar todos los archivos.

---
+++ RTA_SCRIPT_Examen_20241008/Punto_A.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Creamos los directorios.
+#ACLARACIÓN: Usamos SUDO por que solo de esta manera podremos tener los permisos necesarios para escribir dentro de la carpeta raíz(root).
+sudo mkdir -p /Examenes-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
+
---
+++ RTA_SCRIPT_Examen_20241008/ScriptActualizarPermisos.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Este es un script que podrá ejecutar para darle permisos DE EJECUCIÓN a todos los archivos que se encuentren dentro de RTA_SCRIPT_Examen_20241008
+#RECORDATORIO: Ejecutar siempre que se agregue un archivo nuevo
+chmod +x ~/repogit/UTNFRA_SO_1P2C_2024_Sohrobigarat/RTA_SCRIPT_Examen_20241008/*
+
+
