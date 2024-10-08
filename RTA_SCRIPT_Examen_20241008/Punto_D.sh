54a844093e2ef79607c90ab57753fea7c32fc6cb
giulianolol <gino.sohro@yahoo.com.ar>
2024-10-08 18:07:31 +0000

    Add: Punto D resuelto + pequeña explicación en el script

---
+++ RTA_SCRIPT_Examen_20241008/Punto_D.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#A diferencia del punto anterior, acá no es necesario usar el SUDO ya que no estamos modificando la carpeta raíz (root)
+mkdir -p /home/vagrant/Estructura_Asimetrica/{correo/{cartas_{1..100},carteros_{1..10}},clientes/cartas_{1..100}}
+
+#Se agrega un comando para verificar la correcta ejecución
+tree /home/vagrant/Estructura_Asimetrica/ --noreport |pr -T -s'' -w 80 --column 4
