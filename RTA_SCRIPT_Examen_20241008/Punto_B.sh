2dfb5fef8e07331811d534734dd5243f7455f71c
Giuliano Sohrobigarat <gino.sohro@yahoo.com.ar>
2024-10-11 14:18:53 -0300

    ADD: Punto B Finalizado

--- RTA_SCRIPT_Examen_20241008/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241008/Punto_B.sh
@@ -1,39 +1,66 @@
 #!/bin/bash
-sudo fdisk /dev/sdc1
+#CONDICIÓN ANTES DE EJECUTAR ESTE SCRIPT: Ejecutar el script "Punto_A.sh" para crear los directorios donde se van a montar los discos.
+#CREAMOS LAS PARTICIONES
+sudo fdisk /dev/sdc
+n
+p
+
+
++1GB
 n
 e
 
 
+
 n
 
-+1G
++1GB
 n
 
-+1G
++1GB
 n
 
-+1G
++1GB
 n
 
-+1G
++1GB
 n
 
-+1G
++1GB
 n
 
-+1G
++1GB
 n
 
-+1G
++1GB
 n
 
-+1G
++1GB
 n
 
-+1G
-n
-
-+1G
-
++1GB
 w
-lsblk
+#FORMATEAMOS
+sudo partprobe /dev/sdc
+sudo mkfs.ext4 /dev/sdc1
+sudo mkfs.ext4 /dev/sdc5
+sudo mkfs.ext4 /dev/sdc6
+sudo mkfs.ext4 /dev/sdc7
+sudo mkfs.ext4 /dev/sdc8
+sudo mkfs.ext4 /dev/sdc9
+sudo mkfs.ext4 /dev/sdc10
+sudo mkfs.ext4 /dev/sdc11
+sudo mkfs.ext4 /dev/sdc12
+sudo mkfs.ext4 /dev/sdc13
+#Escribimos el fstab para que quede de manera persistente.
+echo "/dev/sdc1 /Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc5 /Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc6 /Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc7 /Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc8 /Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc9 /Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc10 /Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc11 /Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc12 /Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc13 /Examenes-UTN/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
+sudo monut -a
